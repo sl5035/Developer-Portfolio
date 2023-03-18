c5f39fda035afd44041b507d7d6e154da0cbb44f
Robin Lee <sl5035@columbia.edu>
2023-03-18 19:07:28 -0400

    Theme selection option added to navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,13 @@
 <script>
 	import Navbar from '$lib/components/navbar.svelte';
 	import Footer from '$lib/components/footer.svelte';
+	import { onMount } from 'svelte';
+	import { themeChange } from 'theme-change';
 	import '../app.postcss';
+
+	onMount(async () => {
+		themeChange(false);
+	});
 </script>
 
 <Navbar />
