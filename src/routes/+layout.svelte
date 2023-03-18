8f9cdffb6a090d14949e26f03e6861f152605dcd
Robin Lee <sl5035@columbia.edu>
2023-03-18 18:57:29 -0400

    Footer component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
+	import Footer from '$lib/components/footer.svelte';
 	import '../app.postcss';
 </script>
 
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
 </main>
+
+<Footer />
