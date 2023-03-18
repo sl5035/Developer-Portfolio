470ee0a7140ae6ff6dce79040458acfd094283e7
Robin Lee <sl5035@columbia.edu>
2023-03-18 19:01:48 -0400

    Added Navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
+	import Navbar from '$lib/components/navbar.svelte';
 	import Footer from '$lib/components/footer.svelte';
 	import '../app.postcss';
 </script>
+
+<Navbar />
 
 <main class="container max-w-3xl mx-auto px-4 mb-20">
 	<slot />
