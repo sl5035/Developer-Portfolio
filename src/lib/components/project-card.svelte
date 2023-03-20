8f100936ddaf49925d3c0c5a94acf5c326646758
Robin Lee <sl5035@columbia.edu>
2023-03-20 18:24:48 -0400

    project card bug fix

--- src/lib/components/project-card.svelte
+++ src/lib/components/project-card.svelte
@@ -2,7 +2,6 @@
 	export let url = '';
 	export let name = '';
 	export let slug = '';
-	export let description = '';
 </script>
 
 <div class="relative group card shadow-2xl col-span-2">
@@ -12,9 +11,6 @@
 			class="absolute bottom-0 left-0 right-0 lg:opacity-0 group-hover:opacity-100 bg-primary p-4 duration-300 text-primary-content"
 		>
 			<h2 class="font-bold lg:text-xl">{name}</h2>
-			<p class="text-sm lg:text-xl">
-				{description.slice(0, 80)}...
-			</p>
 		</div>
 	</a>
 </div>
